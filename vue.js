075ea6963d00d16071ddf4d1cc87e0e5b9009485
AnjaHrydziushka <anja.hryd@gmail.com>
2020-09-03 16:18:47 +0200

    Added button and cart, @mouseover to change colors

--- vue.js
+++ vue.js
@@ -2,18 +2,29 @@ const app = new Vue({
     el: '#app',
     data: {
         product: "T-shirt",
-        image: "./assets/vyshymaika.jpg",
+        image: "./assets/vyshymaika-white.jpg",
         onSale: true,
         sizes: ["S", "M", "L", "XL"],
         variants: [
             {
                 variantId: 1,
-                variantColor: "black"
+                variantColor: "black",
+                variantImage: "./assets/vyshymaika-black.jpg"
             },
             {
                 variantId: 2,
-                variantColor: "white"
+                variantColor: "white",
+                variantImage: "./assets/vyshymaika-white.jpg"
             }
-        ]
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
